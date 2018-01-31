ede22a5027b46ec26538ebbb89371d52f9635f19
Marc Elosua <elosua.marc@gmail.com>
2018-01-31 10:31:55 +0100

    Dictionary pending snp list as string

--- web_retriever.py
+++ web_retriever.py
@@ -2,18 +2,18 @@ import urllib.request, urllib.parse
 
 data = {
 'batch_text' : '',
-'refseq' : '',
-'ensembl' : '',
-'sift' : '',
-'polyphen' : '',
-'afr' : '',
-'amr' : '',
-'eas' : '',
-'eur' : '',
-'sas' : '',
-'clinvar' : '',
+'refseq' : 'on',
+'ensembl' : 'on',
+'sift' : 'on',
+'polyphen' : 'on',
+'afr' : 'on',
+'amr' : 'on',
+'eas' : 'on',
+'eur' : 'on',
+'sas' : 'on',
+'clinvar' : 'on',
  }
-
+url = 'http://snp-nexus.org'
 data = bytes( urllib.parse.urlencode( data ).encode() )
-handler = urllib.request.urlopen( 'http://snp-nexus.org', data );
+handler = urllib.request.urlopen( url, data );
 print( handler.read().decode( 'utf-8' ) );
