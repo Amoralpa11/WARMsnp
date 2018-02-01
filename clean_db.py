c0a2ba5e7d302c3e3c48030748de7af9c29bafce
Marc Elosua <elosua.marc@gmail.com>
2018-02-01 10:18:59 +0100

    Modified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coordModified the script to contain the chr - ensmblID - start_coord - end_coord

--- clean_db.py
+++ clean_db.py
@@ -1,9 +1,9 @@
-out = open("IDens_coord.txt","w")
-out.write("ensembl_ID" + "\t" + "in_coord" + "\t" + "end_coord" + "\n")
+out = open("../databases/IDens_coord.txt","w")
+out.write("chr" + "\t" + "ensembl_ID" + "\t" + "in_coord" + "\t" + "end_coord" + "\n")
 with open("../databases/Homo_sapiens.GRCh38.91.gtf") as fh:
     for line in fh:
         if "#!" not in line:
             # print(line.split()[2])
 
             if "gene" in line.split()[2]:
-                out.write(line.split()[9][1:len(line.split()[9])-2] + "\t" + line.split()[3] + "\t" + line.split()[4] + "\n")
+                out.write(line.split()[0] + "\t" + line.split()[9][1:len(line.split()[9])-2] + "\t" + line.split()[3] + "\t" + line.split()[4] + "\n")
