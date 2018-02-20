487222c83460e9d4c9fe261bda92e304712433fc
Marc Elosua <elosua.marc@gmail.com>
2018-02-20 16:44:58 +0100

    More and new improvements ;)

--- SNP_out.php
+++ SNP_out.php
@@ -1,155 +1,149 @@
-<!doctype html>
-		<!-- Page Content -->
-		<?php
-		#start Session to hold input data
-		// session_start();
-		include "navbar.html";
+<!-- Page Content -->
+<?php
+#start Session to hold input data
+session_start();
+$_SESSION['queryData'] = $_REQUEST;
+
+if (!isset($_SESSION['queryData']))
+    header('Location: blast_index.php');
 
 
-		# Check if input comes from an uploaded file
-		# If the data comes from a file get the content from the file
-		if ($_FILES['uploadFile']['name']) {
-		    $_REQUEST['query']=  file_get_contents($_FILES['uploadFile']['tmp_name']);
-		}
+include "navbar.html";
+?>
 
-		if ($_REQUEST['maxpval'] > 1 || $_REQUEST['maxpval'] <= 0) { ?>
-			<html>
-					<head>
-							<title>Error: P value out of range</title>
-					</head>
-					<body>
-							<h2>Error: The P value set is out of range, please set it between 0 and 1.</h2>
-					</body>
-			</html>
+<?php
+# Check if input comes from an uploaded file
+# If the data comes from a file get the content from the file
+if ($_FILES['uploadFile']['name']) {
+    $_REQUEST['query']=  file_get_contents($_FILES['uploadFile']['tmp_name']);
+}
 
-		<?php
-		}
+if ($_REQUEST['maxpval'] > 1 || $_REQUEST['maxpval'] <= 0) {
+?>
+<title>Error: P value out of range</title>
+<h2>Error: The P value set is out of range, please set it between 0 and 1.</h2>
 
-		if ($_REQUEST['$minfreq'] < 0 || $_REQUEST['maxfreq'] > 1) { ?>
-			<html>
-					<head>
-							<title>Error: frequency value out of range</title>
-					</head>
-					<body>
-							<h2>Error: The frequency range set is out of range, please set it between 0 and 1.</h2>
-					</body>
-			</html>
+<?php
+}
 
-		<?php
-		}
+if ($_REQUEST['$minfreq'] < 0 || $_REQUEST['maxfreq'] > 1) { ?>
+    <html>
+    <head>
+        <title>Error: frequency value out of range</title>
+    </head>
+    <body>
+    <h2>Error: The frequency range set is out of range, please set it between 0 and 1.</h2>
+    </body>
+    </html>
 
-
-		# if the query is empty show an error saying that there is no request
-
-		if (!$_REQUEST['query']) { ?>
-		<html>
-		    <head>
-		        <title>Error: No request</title>
-		    </head>
-		    <body>
-		        <h2>Error: Received request was empty, please input data for the query.</h2>
-		    </body>
-		</html>
-		<?php
-		} else {
-		# Process input
-		    # Check if this is an ensembl or snp id
-				# if we are working with SNPs.
-		    if (substr($_REQUEST['query'],0,2) === "rs") {
-		        # To be writen
-						print "<h2>we are working with a snp</h2>";
-						?>
-						<html>
-								<head>
-										<style type="text/css">
-												thead {background-color: #cccccc;color:#ffffff}
-												tbody {background-color: #ffffff};
-										</style>
-										<link rel="stylesheet" href="DataTable/jquery.dataTables.min.css"/>
-										<script type="text/javascript" src="DataTable/jquery-2.2.0.min.js"></script>
-										<script type="text/javascript" src="DataTable/jquery.dataTables.min.js"></script>
-										<head>
-											<body>
-												<h1><?php print $_REQUEST['query']?></h1>
-												<table style="width:100%">
-													<tr>
-														<th>Gene</th>
-														<th>Position</th>
-														<th>Variant frequency</th>
-														<th>Beta</th>
-														<th>p value</th>
-														<th>Disease</th>
-													</tr>
-													<tr>
-														<td>SELECT Gene_id FROM GENE WHERE Gene_id = (SELECT ID FROM SNP WHERE idSNP = $_REQUEST['query'])</td>
-														<td>SELECT position FROM SNP WHEN idSNP = $_REQUEST['query']"</td>
-														<td>SELECT Frequency FROM Variants WHEN idSNP = $_REQUEST['query']"</td>
-														<td>SELECT beta FROM Variants WHEN idSNP = $_REQUEST['query']"</td>
-														<td>SELECT p-value FROM Variants WHEN idSNP = $_REQUEST['query']"</td>
-														<td>SELECT Name FROM Disease WHEN SNP.idSNP = $_REQUEST['query']"</td>
-														<td>SELECT Name FROM Disease WHERE idDisease = (SELECT idDisease FROM SNP-disease WHERE idSNP = $_REQUEST['query'])</td>
-													</tr>
-													<!-- <tr>
-														<td>Eve</td>
-														<td>Jackson</td>
-														<td>94</td>
-													</tr> -->
-												</table>
-											</body>
-										</head>
-				<?php
-				# if we are working with genes.
-		    } else {
-						# To be writen
-						print "<h2>we are working with a gene!</h2>";
-		    }
-		    $_SESSION['data'] = parse_Fasta($fasta);
-		}
-		?>
+    <?php
+}
 
 
-		<script type="text/javascript">
-		    $(document).ready(function () {
-		        $('#dataTable').DataTable();
-		    });
-		</script>
+# if the query is empty show an error saying that there is no request
 
-		<?php
-		function isFasta($f) {
-		    return (substr($f,0,1) == ">");
-		}
+if (!$_REQUEST['query']) { ?>
+    <html>
+    <head>
+        <title>Error: No request</title>
+    </head>
+    <body>
+    <h2>Error: Received request was empty, please input data for the query.</h2>
+    </body>
+    </html>
+    <?php
+} else {
+# Process input
+# Check if this is an ensembl or snp id
+# if we are working with SNPs.
+if (substr($_REQUEST['query'],0,2) === "rs") {
+# To be writen
+// print "<h2>we are working with a snp</h2>";
+// print var_dump($_REQUEST['query']);
+?>
+<html>
+<head>
 
-		function parse_Fasta ($f) {
-		    $sqs = explode(">", $f);
-		    $data=Array();
-		    foreach ($sqs as $s) {
-		        if ($s) {
-		            $lines = explode("\n",$s,2);
-		            list($db,$id,$info) = explode("|",$lines[0]);
-		            list ($swp,$info) = explode(" ",$info,2);
-		            $data[$id] = array('db'=> $db, 'id'=> $id, 'swpid' => $swp, 'info' => $info, 'sequence' => $lines[1]);
-		        }
-		    }
-		    return $data;
-		}
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		echo "<br>";
-		include "footer.html";
+    <link rel="stylesheet" href="DataTable/jquery.dataTables.min.css"/>
+    <script type="text/javascript" src="DataTable/jquery-2.2.0.min.js"></script>
+    <script type="text/javascript" src="DataTable/jquery.dataTables.min.js"></script>
+</head>
+</html>
 
-		?>
 
-  	<!-- Optional JavaScript -->
-  	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  	<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
-  <!-- </body> -->
-  </html>
+<div class="container">
+<h1>RESULTS:</h1>
+<table border="0" cellspacing="2" cellpadding="4" id="blastTable">
+    <thead>
+        <tr>
+          <th>Chromosome</th>
+          <th>Gene</th>
+          <th>Position</th>
+          <th>Variant frequency</th>
+          <th>Beta</th>
+          <th>p value</th>
+          <th>Disease</th>
+          <th>SIFT prediction</th>
+          <th>SIFT score</th>
+          <th>PolyPhen prediction</th>
+          <th>Polyphen score</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $result_arr = preg_split("/\s+/", $_REQUEST['query']);
+        foreach (array_values($result_arr) as $rr) {
+          $chromosome = "probably 1";
+          $gene = "hehe";
+          $position = "don't know";
+          $frequency = "not too high";
+          $beta = "positive";
+          $pval = "so low";
+          $disease = "you fucked";
+          $sift = "missense";
+          $sift_score = "999";
+          $polyphen =  "buff";
+          $polyphen_score = "buff2";
+
+          ?>
+          <tr>
+            <td> <?php print $gene ?> </td>
+            <td> <?php print $position ?> </td>
+            <td> <?php print $chromosome ?> </td>
+            <td> <?php print $frequency ?> </td>
+            <td> <?php print $beta ?> </td>
+            <td> <?php print $pval ?> </td>
+            <td> <?php print $disease ?> </td>
+            <td> <?php print $sift ?> </td>
+            <td> <?php print $sift_score ?> </td>
+            <td> <?php print $polyphen ?> </td>
+            <td> <?php print $polyphen_score ?> </td>
+
+          </tr>
+          <?php
+        }
+
+        ?>
+    </tbody>
+</table>
+</div>
+<?php
+# if we are working with genes.
+} else {
+    # To be writen
+    print "<h2>we are working with a gene!</h2>";
+}
+}
+?>
+
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#blastTable').DataTable();
+});
+</script>
+
+<?php
+include "footer.html";
+?>
+
+</html>
--- WARMsnp_home.php
+++ WARMsnp_home.php
@@ -1,88 +1,98 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>
-
 <?php
+
 include "navbar.html";
-// define variables and set to empty values
+//setting default values
+if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
+    $_SESSION['queryData'] = [
+        'query' => '',
+        'minbeta' => '0',
+        'maxpval' => '0.05',
+        'maxfreq' => '1',
+        'minfreq' => '0'];
+}
+
+if (isset($_POST['query'])) {
+    $_SESSION['queryData']['query'] = $_POST['query'];
+}
+
 ?>
 
 <!-- Page Content -->
 <html>
-    <head>
-        <title>WARMsnp</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <h2>WARMsnp</h2>
-        <p>WARMsnp is a web app designed by and for researchers. It is aimed to ease the SNP information search, it allows you to get SNP related information consolidated from several other web sties.</p>
+<head>
+    <title>WARMsnp</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<div class="container">
+    <h2>WARMsnp</h2>
+    <p>WARMsnp is a web app designed by and for researchers. It is aimed to deliver SNP enriched information, it allows you to get SNP related information consolidated from several other web sties.</p>
 
-        <form action="psnp.php" id="input" name="input" method="GET" enctype="multipart/form-data">
-            <p><em>Enter gene ensembl id or snp id:</em></p>
-            <textarea name="query" cols="40" rows="3" placeholder="ensemble id or snp id" style="margin-left: 1%;"></textarea><br>
-            <span class="error"><?php echo $nameErr?></span>
+    <form action="SNP_out.php" id="input" name="input" method="GET" enctype="multipart/form-data">
+      <fieldset class="advanced search">
+        <p><em>Enter gene ensembl id or snp id:</em></p>
+        <textarea name="query" cols="40" rows="3" placeholder="ensemble id or snp id" style="margin-left: 1%;"></textarea><br>
+        <span class="error"><?php echo $nameErr?></span>
 
-            <input name="uploadFile" type="file" style="margin-left: 1%;"><br>
-            <br>
-            <!-- <input type="submit" value="Send data"> <input type="reset" value="Clear data"> -->
+        <input name="uploadFile" type="file" style="margin-left: 1%;"><br>
+        <br>
+        <!-- <input type="submit" value="Send data"> <input type="reset" value="Clear data"> -->
 
         <!-- </form> -->
 
-    <!-- </body>
-    <head>
-      <body> -->
+        <!-- </body>
+        <head>
+          <body> -->
         <h3>Advanced search</h3>
-        <p>Select the parameters to further filter the search:</p>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label>beta</label>
-                    <p>
-                        Higher than: <input type="text" name="minbeta" value="<?php print $_SESSION['queryData']['minbeta'] ?>" size="5">
-                        <!-- to <input type="text" name="maxRes" value="<*?php print $_SESSION['queryData']['maxRes'] ?>" size="5" > -->
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label>p value</label>
-                    <p>
-                        <!-- Lower than: <input type="text" name="maxpval" value="<*?php print $_SESSION['queryData']['maxpval'] ?>" size="5"> -->
-                        Lower than: <input type="text" name="maxpval" value="1" size="5">
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label>variant frequency</label>
-                    <p>
-                      <!-- from: <input type="text" name="minfreq" value="<*?php print $_SESSION['queryData']['minfreq'] ?>" size="5">
-                      to: <input type="text" name="maxfreq" value="<*?php print $_SESSION['queryData']['maxfreq'] ?>" size="5" > -->
-                      from: <input type="text" name="minfreq" value="0" size="5">
-                      to: <input type="text" name="maxfreq" value="1" size="5" >
+        <div>
+            <p>Select the parameters to further filter the search:</p>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label>beta</label>
+                        <p>
+                            Higher than:
+                            <input type="text" name="minbeta" value= <?php print $_SESSION['queryData']['minbeta'] ?> size="5">
 
-                    </p>
+                        </p>
+                    </div>
                 </div>
-            </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label>p value</label>
+                        <p>
+                            Lower than: <input type="text" name="maxpval" value= <?php print $_SESSION['queryData']['maxpval'] ?> size="5">
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label>variant frequency</label>
+                        <p>
+                            from: <input type="text" name="minfreq" value=" <?php print $_SESSION['queryData']['minfreq'] ?>" size="5">
+                            to: <input type="text" name="maxfreq" value= <?php print $_SESSION['queryData']['maxfreq'] ?> size="5" >
+
+                        </p>
+                    </div>
+                </div>
                 <input type="submit" value="Send data" style="margin-left: 2%; margin-bottom: 2%"> <input type="reset" value="Clear data" style="margin-bottom: 2%"><br>
                 <br>
-        </div>
-      </form>
-    </body>
-    <!-- </head> -->
-  </html>
+            </div>
+          </div>
+        </fieldset>
+    </form>
+  </div>
+  </body>
+</html>
 
-  <?php include "footer.html" ?>
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- Optional JavaScript -->
+<!-- DataTable activation -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<?php include "footer.html"?>
 </body>
 </html>
