18f8b9f848832ad702aac4b302c022da806630c7
Amoralpa11 <drnmoralespastor@gmail.com>
2018-02-23 10:49:50 +0100

    Creado SNP_page y arreglado size = 5 y isnumerci de WARMsnp

---
+++ SNP_page.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+include "navbar.html";				#incluimos la barra de navegación y el head de la pagina
+include 'databasecon.php';			#incluimos la página en la que nos conectamos con la base de datos 
+
+session_start();
+
+ 
