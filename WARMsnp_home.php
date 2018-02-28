7702854d05deffe4ec5877ac4fe3903259eb395e
Marc Elosua <elosua.marc@gmail.com>
2018-02-28 16:57:26 +0100

    Fixed JS and added features in the home page

--- WARMsnp_home.php
+++ WARMsnp_home.php
@@ -14,28 +14,6 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
         'freq_error' => '0',
         'query_error' => '0'];
 }
-
-
-
-if ($_SESSION['queryData']['query_error'] = "1") {
-  $query_err = "Input text or file are required,";
-} else {
-  $query_err = '';
-}
-
-if ($_SESSION['queryData']['p_error'] = "1") {
-  $p_err = "* P value is out of range, please set it between 0 and 1.";
-} else {
-  $p_err = '';
-
-}
-
-if ($_SESSION['queryData']['freq_error'] = "1") {
-  $freq_err = "* Frequencies are set out of range, please set them between 0 and 1.";
-} else {
-  $freq_err = '';
-
-}
 ?>
 
 <!-- Page Content -->
@@ -45,22 +23,24 @@ if ($_SESSION['queryData']['freq_error'] = "1") {
     <meta charset="UTF-8">
 </head>
 <body>
+  <br>
 <div class="container">
+  <div class="row">
+    <div class="col-md-8">
     <h2>WARMsnp</h2>
     <p>WARMsnp is a web app designed by and for researchers. It is aimed to deliver SNP enriched information, it allows you to get SNP related information consolidated from several other web sties.</p>
 
     <form id="input_form" name="input" action="SNP_out.php" method="post" enctype="multipart/form-data">
         <p><em>Enter gene ensembl id or snp id:</em></p>
-        <textarea id="text_in" name="query" cols="40" rows="3" placeholder="ensemble id or snp id" style="margin-left: 1%;"><?php print $_SESSION["queryData"]["query"]?></textarea><span id="query_not_type_message" class="error" style="color:#ff0000; display:none; margin-left: 2%; margin-bottom:30px;">Input text must be a gene ensembl id (ej:"ens000001") or a snp id (ej:"rs000001")</span>
+        <textarea id="text_in" name="query" cols="40" rows="3" placeholder="ensemble id or snp id" style="margin-left: 1%;"><?php print $_SESSION["queryData"]["query"]?></textarea><span id="query_not_type_message" class="error" style="color:#ff0000; display:none; margin-left: 2%; margin-bottom:30px;">* Input text must be a a gene ensembl id (e.g. ENS0001 or a snp id (e.g. rs0001)</span>
         <br><input id="file_in"name="uploadFile" type="file" style="margin-left: 1%;">
           <span class="error" style="color:#ff0000;">*</span>
           <span id="query_err_message" class="error" style="color:#ff0000; display:none;">Input text or file are required</span><br>
-
         <br>
-        <fieldset class="advanced search">
-        <h3>Advanced search</h3>
-        <div>
-            <p>Select the parameters to further filter the search:</p>
+        <fieldset class="advanced search" style="border: 1px dashed black;">
+        <h3 style="margin-left: 15px; margin-top: 5px;">Advanced search</h3>
+        <div style="margin-left: 30px;">
+            <p>Select parameters to further filter the search:</p>
             <div class="row">
                 <div class="col-md-4">
                     <div class="form-group">
@@ -90,7 +70,7 @@ if ($_SESSION['queryData']['freq_error'] = "1") {
                             from: <input type="text" id="minfreq" name="minfreq" value='<?php print $_SESSION['queryData']['minfreq'] ?>' size="5">
                             to: <input type="text" id="maxfreq" name="maxfreq" value= '<?php print $_SESSION['queryData']['maxfreq'] ?>' size="5" >
                             <span id="freq_error" class="error" style="color:#ff0000; display:none;">* Frequencies are set out of range, please set them between 0 and 1.</span>
-                            <span id="freq_inv" class="error" style="color:#ff0000; display:none;">* Minimum frequency is bigger than maximum frequency.</span>
+                            <span id="freq_inv" class="error" style="color:#ff0000; display:none;">* Minimum frequency is bigger than maximum frequency.</span><br>
                             <span id="freq_num" class="error" style="color:#ff0000; display:none;">* Minimum frequency and maximum frequencies must be numbers.</span>
 
                         </p>
@@ -106,8 +86,75 @@ if ($_SESSION['queryData']['freq_error'] = "1") {
     <button onclick="reset()">Reset</button><br><br>
 
 
-
+   </div>
+   <div class="col-md-4", style="border: 1px solid black; margin-bottom: 25px; overflow:scroll; height: 450px;">
+     <h3>Interesting facts</h3>
+     <p style=" margin-right: 5px;">
+       <ul>
+        <li>
+          Here is a side column where we can write interesting facts:
+        Which is the gene with the most snps,
+        which is the snp with the highest beta,
+        Which is disease with the most snps,
+      </li>
+      <hr>
+      <li>
+          Here is a side column where we can write interesting facts:
+        Which is the gene with the most snps,
+        which is the snp with the highest beta,
+        Which is disease with the most snps,
+      </li>
+      <hr>
+      <li>
+          Here is a side column where we can write interesting facts:
+        Which is the gene with the most snps,
+        which is the snp with the highest beta,
+        Which is disease with the most snps,
+      </li>
+      <hr>
+      <li>
+          Here is a side column where we can write interesting facts:
+        Which is the gene with the most snps,
+        which is the snp with the highest beta,
+        Which is disease with the most snps,
+      </li>
+      <hr>
+      <li>
+        Here is a side column where we can write interesting facts:
+      Which is the gene with the most snps,
+      which is the snp with the highest beta,
+      Which is disease with the most snps,
+    </li>
+    <hr>
+    <li>
+        Here is a side column where we can write interesting facts:
+      Which is the gene with the most snps,
+      which is the snp with the highest beta,
+      Which is disease with the most snps,
+    </li>
+    <hr>
+    <li>
+        Here is a side column where we can write interesting facts:
+      Which is the gene with the most snps,
+      which is the snp with the highest beta,
+      Which is disease with the most snps,
+    </li>
+    <hr>
+    <li>
+        Here is a side column where we can write interesting facts:
+      Which is the gene with the most snps,
+      which is the snp with the highest beta,
+      Which is disease with the most snps,
+    </li>
+    <hr>
+    <li>Tea</li>
+    <hr>
+    <li>Milk</li>
+      </ul>
+   </p>
+   </div>
   </div>
+</div>
 </body>
 </html>
 
@@ -135,20 +182,20 @@ function reset() {
 
 function check() {
     var okay = 0;
-
-    if ($("#minfreq").val() < 0 || $("#minfreq").val() > 1 || $("#maxfreq").val() > 1 || $("#maxfreq").val() < 0) {
-      $("#freq_error").show();
-      okay = 1;
-    } else {
-      $("#freq_error").hide();
-    }
-    if ($("#minfreq").val() > $("#maxfreq").val()) {
-      $("#freq_inv").show();
-      okay = 1;
-    } else {
-      $("#freq_inv").hide();
-    if ($("#minfreq").val().isNumeric() && $("#maxfreq").val().isNumeric()) {
+    if ($.isNumeric( $("#minfreq").val() ) && $.isNumeric( $("#maxfreq").val() )) {
       $("#freq_num").hide();
+      if ($("#minfreq").val() < 0 || $("#minfreq").val() > 1 || $("#maxfreq").val() > 1 || $("#maxfreq").val() < 0) {
+        $("#freq_error").show();
+        okay = 1;
+      } else {
+        $("#freq_error").hide();
+        if ($("#minfreq").val() > $("#maxfreq").val()) {
+          $("#freq_inv").show();
+          okay = 1;
+        } else {
+          $("#freq_inv").hide();
+        }
+      }
     } else {
       $("#freq_num").show();
       okay = 1;
@@ -158,18 +205,6 @@ function check() {
       okay = 1;
     } else {
       $("#pval_err").hide();
-    }
-    if ($("#maxpval").val().isNumeric()) {
-      $("#pval_num").hide();
-    } else {
-      $("#pval_num").show();
-      okay = 1;
-    }
-    if ($("#minbeta").val().isNumeric()) {
-      $("#beta_num").hide();
-    } else {
-      $("#beta_num").show();
-      okay = 1;
     }
     if ($("#text_in").val() == '' && $("#file_in").val() == '') {
       $("#query_err_message").show();
@@ -181,6 +216,18 @@ function check() {
       $("#query_not_type_message").hide();
     } else {
       $("#query_not_type_message").show();
+      okay = 1;
+    }
+    if ( $.isNumeric( $("#minbeta").val() ) ) {
+      $("#beta_num").hide();
+    } else {
+      $("#beta_num").show();
+      okay = 1;
+    }
+    if ($.isNumeric( $("#maxpval").val() )) {
+      $("#pval_num").hide();
+    } else {
+      $("#pval_num").show();
       okay = 1;
     }
     if (okay == 0) {
