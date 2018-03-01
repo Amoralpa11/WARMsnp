15cf7235f76bfecd0e2444dbe83810d18857941f
Marc Elosua <elosua.marc@gmail.com>
2018-03-01 18:08:56 +0100

    Home page updated with more curiosities and attempt 2 to put gif while loading snp out page..

--- SNP_out.php
+++ SNP_out.php
@@ -103,6 +103,8 @@ $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
 </head>
 <body>
 
+<div class="se-pre-con"></div>
+
 <div class="container">
 <h1>RESULTS:</h1>
 <table border="0" cellspacing="2" cellpadding="4" id="blastTable">
@@ -157,22 +159,23 @@ $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
 </table>
 </div>
 
-
-
-
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
 
 <script type="text/javascript">
-$(document).ready(function () {
-    $('#blastTable').DataTable();
-});
-</script>
-
-<script>
+//paste this code under the head tag or in a separate js file.
+// Wait for window load
 $(window).load(function() {
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");;
 });
 
+</script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+    $('#blastTable').DataTable();
+});
 </script>
 
 <?php
--- WARMsnp_home.php
+++ WARMsnp_home.php
@@ -6,13 +6,10 @@ session_start();
 if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
     $_SESSION['queryData'] = [
         'query' => '',
-        'minbeta' => '0',
-        'maxpval' => '0.05',
-        'maxfreq' => '1',
-        'minfreq' => '0',
-        'p_error' => '0',
-        'freq_error' => '0',
-        'query_error' => '0'];
+        'minbeta' => '',
+        'maxpval' => '',
+        'maxfreq' => '',
+        'minfreq' => ''];
 }
 ?>
 
@@ -23,37 +20,28 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
     <meta charset="UTF-8">
 </head>
 <body>
-  <div class="se-pre-con"></div>
+  <!-- <div class="se-pre-con"></div> -->
   <br>
 <div class="container">
   <div class="row">
     <div class="col-md-8">
     <h2>WARMsnp</h2>
-    <p style="text-align: justify; text-justify: inter-word;">
-      WARMsnp is a web app designed by and for researchers. It is aimed to deliver SNP enriched information, it allows you to get SNP related information consolidated from several other web sties.
-    </p>
+    <p style="text-align: justify; text-justify: inter-word;">WARMsnp is a web app designed by and for researchers. It is aimed to deliver SNP enriched information, it allows you to get SNP related information consolidated from several other web sties.</p>
 
     <form id="input_form" name="input" action="SNP_out.php" method="post" enctype="multipart/form-data">
-        <p><em>Enter gene ensembl id or snp id:</em></p>
-        <textarea id="text_in" name="query" cols="40" rows="3" placeholder="ensemble id or snp id" style="margin-left: 1%;">
-          <?php print $_SESSION["queryData"]["query"]?>
-        </textarea>
-        <span id="query_not_type_message" class="error" style="color:#ff0000; display:none; margin-left: 2%; margin-bottom:30px;">
-          * Input text must be a a gene ensembl id (e.g. ENS0001 or a snp id (e.g. rs0001)
-        </span>
-        <br><input id="file_in"name="uploadFile" type="file" style="margin-left: 1%;">
-          <span id="query_err_message" class="error" style="color:#ff0000; display:none;">
-            * Input text or file are required
-          </span><br>
+      <div style="border: 1px dashed">
+        <p style="margin-left:25px; margin-top:5px"><em>Enter gene ensembl id or snp id:</em></p>
+        <textarea id="text_in" name="query" cols="40" rows="3" placeholder="ensemble id or snp id" style="margin-left: 35px;"><?php print $_SESSION["queryData"]["query"]?></textarea>
+        <span id="query_not_type_message" class="error" style="color:#ff0000; display:none; margin-left: 2%; margin-bottom:30px;">* Input text must be a a gene ensembl id (e.g. ENS0001 or a snp id (e.g. rs0001)</span><br>
+        <span id="perc_doll" class="error" style="color:#ff0000; display:none;">* Input text or file can't contain $ or % signs.</span>
+        <br><input id="file_in"name="uploadFile" type="file" style="margin-left:35px; margin-bottom:15px">
+        <span id="query_err_message" class="error" style="color:#ff0000; display:none;">* Input text or file are required</span><br>
+      </div>
         <br>
-        <fieldset class="advanced search" style="border: 1px dashed black;">
-        <h3 style="margin-left: 15px; margin-top: 5px;">
-          Advanced search
-        </h3>
+        <fieldset id="advanced_search" class="advanced search" style="border: 1px dashed black; display:none;">
+        <h3 style="margin-left: 15px; margin-top: 5px;">Advanced search</h3>
         <div style="margin-left: 30px;">
-            <p>
-              Select parameters to further filter the search:
-            </p>
+            <p>Select parameters to further filter the search:</p>
             <div class="row">
                 <div class="col-md-4">
                     <div class="form-group">
@@ -61,9 +49,7 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
                         <p>
                             Higher than:
                             <input type="text" name="minbeta"id="minbeta" value= "<?php print $_SESSION['queryData']['minbeta'] ?>" size="5">
-                            <span id="beta_num" class="error" style="color:#ff0000; display:none;">
-                              * Beta must be a number.
-                            </span>
+                            <span id="beta_num" class="error" style="color:#ff0000; display:none;">* Beta must be a number.</span>
                         </p>
                     </div>
                 </div>
@@ -72,34 +58,22 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
                         <label style="font-weight: bold;">P value</label>
                         <p>
                             Lower than: <input type="text" id="maxpval" name="maxpval" value= "<?php print $_SESSION['queryData']['maxpval'] ?>" size="5">
-                            <span id="pval_err" class="error" style="color:#ff0000; display:none;">
-                              * P value is out of range, please set it between 0 and 1.
-                            </span>
-                            <span id="pval_num" class="error" style="color:#ff0000; display:none;">
-                              * P value must be a number.
-                            </span>
+                            <span id="pval_err" class="error" style="color:#ff0000; display:none;">* P value is out of range, please set it between 0 and 1.</span>
+                            <span id="pval_num" class="error" style="color:#ff0000; display:none;">* P value must be a number.</span>
                         </p>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
-                        <label style="font-weight: bold;">
-                          Variant Frequency
-                        </label>
+                        <label style="font-weight: bold;">Variant Frequency</label>
                         <p>
                             From: <input type="text" id="minfreq" name="minfreq" value='<?php print $_SESSION['queryData']['minfreq'] ?>' size="5">
                             To: <input type="text" id="maxfreq" name="maxfreq" value= '<?php print $_SESSION['queryData']['maxfreq'] ?>' size="5" >
-                            <span id="freq_error" class="error" style="color:#ff0000; display:none;">
-                              * Frequencies are set out of range, please set them between 0 and 1.
-                            </span>
-                            <span id="freq_inv" class="error" style="color:#ff0000; display:none;">
-                              * Minimum frequency is bigger than maximum frequency.
-                            </span><br>
-                            <span id="freq_num" class="error" style="color:#ff0000; display:none;">
-                              * Minimum frequency and maximum frequencies must be numbers.
-                            </span>
-
+                            <span id="freq_error" class="error" style="color:#ff0000; display:none;">* Frequencies are set out of range, please set them between 0 and 1.</span>
+                            <span id="freq_inv" class="error" style="color:#ff0000; display:none;">* Minimum frequency is bigger than maximum frequency.</span><br>
+                            <span id="freq_num" class="error" style="color:#ff0000; display:none;">* Minimum frequency and maximum frequencies must be numbers.</span>
                         </p>
+                        <span id="perc_doll_adv" class="error" style="color:#ff0000; display:none;">* Advanced values can't contain $ or % symbols.</span>
                     </div>
                 </div>
                 <br>
@@ -107,7 +81,9 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
           </div>
         </fieldset>
     </form>
-
+    <button onclick="AS()" style="border-radius: 12px;background-color: #e9e9e6; margin-bottom:25px">
+      Advanced Search
+    </button><br>
     <button onclick="check()" style="border-radius: 12px;background-color: #e9e9e6">
       Submit
     </button>
@@ -121,7 +97,7 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
       <h3 style="text-align: center;">
         Interesting facts
       </h3>
-        <div style="border: 1px solid black; overflow:scroll; height: 410px">
+        <div style="border: 1px solid black; overflow:scroll; height: 420px">
          <ul style="margin-top: 15px; text-align: justify; text-justify: inter-word;margin-right: 25px;">
           <li>
             If you were to recite the entire ATCG sequence, pronouncing each of its 3 billion letters the genetic material notation is made of at a rate of 100 ATCG sequences per minute without sleeping, eating or drinking, you would cite for 57 years.
@@ -132,24 +108,15 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
           </li>
           <hr>
           <li>
-            Here is a side column where we can write interesting facts:
-          Which is the gene with the most snps,
-          which is the snp with the highest beta,
-          Which is disease with the most snps,
+            The Platypus, <a style="font-style: italic">Ornithorhyncus anatinus</a>, has five pairs of sex chromosomes, one being similar to humans and another to birds!
           </li>
           <hr>
           <li>
-            Here is a side column where we can write interesting facts:
-          Which is the gene with the most snps,
-          which is the snp with the highest beta,
-          Which is disease with the most snps,
+            <a style="font-style: italic">Galdieria sulphuraria</a>, an algae, is able to perform horizontal gene transfer despite being a pluricellular organism!
           </li>
           <hr>
           <li>
-            Here is a side column where we can write interesting facts:
-          Which is the gene with the most snps,
-          which is the snp with the highest beta,
-          Which is disease with the most snps,
+            <a style="font-style: italic">Paris japonica</a>, an octoploid mountain flower, has one of the largest, if not largest, genomes of all: 149 giga base pairs! Or what is the same, 50 times larger than the human genome!!!
         </li>
         <hr>
         <li>
@@ -194,21 +161,27 @@ if (isset($_REQUEST['new']) or !isset($_SESSION['queryData'])) {
 
 <script>
 function reset() {
-    $("#minfreq").val('0');
-    $("#maxfreq").val('1');
-    $("#minbeta").val('0');
-    $("#maxpval").val('0.05');
+    $("#minfreq").val('');
+    $("#maxfreq").val('');
+    $("#minbeta").val('');
+    $("#maxpval").val('');
     $("#text_in").val('');
-
 }
-
 </script>
 
 <script>
-
 function check() {
     var okay = 0;
-    if ($.isNumeric( $("#minfreq").val() ) && $.isNumeric( $("#maxfreq").val() )) {
+    if ( !$("#minfreq").val() || !$("#maxfreq").val() ) {
+      if ( $("#minfreq").val() == '' ) {
+        $("#minfreq").val('0');
+      }
+      if ($("#maxfreq").val() == ''){
+        $("#maxfreq").val('1');
+      }
+    }
+
+    if ( $.isNumeric( $("#minfreq").val() ) && $.isNumeric( $("#maxfreq").val() ) ) {
       $("#freq_num").hide();
       if ($("#minfreq").val() < 0 || $("#minfreq").val() > 1 || $("#maxfreq").val() > 1 || $("#maxfreq").val() < 0) {
         $("#freq_error").show();
@@ -226,12 +199,18 @@ function check() {
       $("#freq_num").show();
       okay = 1;
     }
+
+    if ( !$("#maxpval").val() ) {
+      $("#maxpval").val('1')
+    }
+
     if ($("#maxpval").val() < 0 || $("#maxpval").val() > 1) {
       $("#pval_err").show();
       okay = 1;
     } else {
       $("#pval_err").hide();
     }
+
     if ($("#text_in").val() == '' && $("#file_in").val() == '') {
       $("#query_err_message").show();
       okay = 1;
@@ -244,21 +223,60 @@ function check() {
         okay = 1;
       }
     }
-    if ( $.isNumeric( $("#minbeta").val() ) ) {
+
+    if ( $.isNumeric( $("#minbeta").val() ) || !$("#minbeta").val() ) {
       $("#beta_num").hide();
     } else {
       $("#beta_num").show();
       okay = 1;
     }
+
     if ($.isNumeric( $("#maxpval").val() )) {
       $("#pval_num").hide();
     } else {
       $("#pval_num").show();
       okay = 1;
     }
+
+    if ( /(\$|%)/.test( $("#text_in").val() ) ) {
+      $("#perc_doll").show();
+      okay = 1;
+    } else {
+      $("#perc_doll").hide();
+    }
+
     if (okay == 0) {
       $("#input_form").submit();
     }
+
+    if ( /(\$|%)/.test( $("#minbeta").val() ) || /(\$|%)/.test( $("#maxpval").val() ) || /(\$|%)/.test( $("#maxfreq").val() ) || /(\$|%)/.test( $("#minfreq").val() ) ) {
+      $("#perc_doll_adv").show();
+      okay = 1;
+    } else {
+      $("#perc_doll_adv").hide();
+    }
+
+    if (okay == 0) {
+      $("#input_form").submit();
+    }
+
+}
+</script>
+
+<script>
+var i = 0;
+function AS(){
+  i = i + 1;
+  if (i%2 == 0 ) {
+    $("#advanced_search").hide();
+  } else {
+    $("#advanced_search").show();
+    $("#minbeta").val(<?php print $_SESSION['queryData']['minbeta'] ?>);
+    $("#maxpval").val(<?php print $_SESSION['queryData']['maxpval'] ?>);
+    $("#minfreq").val(<?php print $_SESSION['queryData']['minfreq'] ?>);
+    $("#maxfreq").val(<?php print $_SESSION['queryData']['maxfreq'] ?>);
+
+  }
 }
 </script>
 
