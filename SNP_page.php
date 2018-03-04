86a2e3e04b746b637544cd52f2262c63974d2ce4
Marc Elosua <elosua.marc@gmail.com>
2018-03-04 13:39:49 +0100

    Changed the title of the tab

--- SNP_page.php
+++ SNP_page.php
@@ -1,16 +1,33 @@
-<?php 
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="scss/custom.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+
+
+	<title>SNP results</title>
+</head>
+
+<?php
 
 
 include "navbar.html";				#incluimos la barra de navegación y el head de la pagina
-include 'databasecon.php';			#incluimos la página en la que nos conectamos con la base de datos 
+include 'databasecon.php';			#incluimos la página en la que nos conectamos con la base de datos
 
 session_start();
 
 $_SESSION['SNP_page'] = $_REQUEST;
 
-$sql = "select    * 
+$sql = "select    *
         from      SNP as s, Gene as g ,
-                  Gene_has_SNP as gs, Variants as v 
+                  Gene_has_SNP as gs, Variants as v
         where     s.idSNP = gs.SNP_idSNP and
                   gs.Gene_Gene_id = g.Gene_id and
                   v.idSNP = s.idSNP
