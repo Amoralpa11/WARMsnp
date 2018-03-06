ac913be1bbc3e6594b2b0db6870f39420d002c52
Amoralpa11 <drnmoralespastor@gmail.com>
2018-03-06 13:08:02 +0100

    Cambiadas creedenciales base de datos

--- databasecon.php
+++ databasecon.php
@@ -8,8 +8,8 @@
 /*Definimos las variables necesarias para establecer la conexión con la base de datos*/
 $host = "localhost";
 $dbname = "WARMsnp";
-$user = "root";
-$password = "";
+$user = "Winonaladrona";
+$password = "WinnieMouse";
 
 /*También guardamos en una variable la string del inicio de las querys para mysql, donde nos conectamos a la base de datosl*/
 ($mysqli = mysqli_connect($host, $user, $password)) or die(mysqli_error());
